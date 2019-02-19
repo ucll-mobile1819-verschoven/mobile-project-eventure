58d0c63c77dbacc6ecfa5572c9986909d87d5ec3
Olitop2.0 <oliver.obrien@student.ucll.be>
2019-02-19 17:24:20 +0100

    Test to see if there is any problems with pulling new code

--- Eventure/App.js
+++ Eventure/App.js
@@ -5,6 +5,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+            <Text>This is a Test</Text>
+          </View>
+          <View style={{flex: 3, backgroundColor: 'skyblue'}}>
+              <Text>To see</Text>
+          </View>
+          <View style={{flex: 4, backgroundColor: 'steelblue'}}>
+          <Text>how shit changes</Text>
+      </View>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
